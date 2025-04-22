4b145e45ab6a13a6049b9ca8e8228f58e29fe5be
swathi kondapalli <swathikondapalli@swathis-MBP.lan>
2025-04-22 11:39:43 -0500

    Generated code for: Create a function to print given number of Fibonacci numbers

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,18 +1,31 @@
-Here's a Python function that prints the Fibonacci series up to a given number.
+Sure, here is a simple Python function that prints the Fibonacci series up to the given number of terms:
 
 ```python
-def print_fibonacci(n):
-    a, b = 0, 1  # fibonacci initial two numbers
-
-    while a < n:
-        print(a, end=' ')
-        a, b = b, a+b  # generate next fibonacci number
-
-    print()  # print newline
-
-print_fibonacci(100)
+def print_fibonacci(n_terms):
+    if n_terms <= 0:
+        print("Please provide a positive integer.")
+    elif n_terms == 1:
+        print("Fibonacci sequence up to", n_terms, ":")
+        print(0)
+    else:
+        print("Fibonacci sequence up to", n_terms, ":")
+        term1, term2 = 0, 1
+        count = 0
+        while count < n_terms:
+            print(term1)
+            next_term = term1 + term2
+            # swap values
+            term1 = term2
+            term2 = next_term
+            count += 1
 ```
 
-When you run this program, it outputs the Fibonacci numbers that are less than the input number (in this case, 100).
+You can use the function `print_fibonacci(n_terms)` where `n_terms` is the number of terms in the Fibonacci sequence you want to print. For example, `print_fibonacci(5)` will print the first five terms of the Fibonacci sequence:
 
-Remember that the Fibonacci series is a sequence of numbers in which each number is the sum of the two preceding ones, usually starting with 0 and 1.
+```
+0
+1
+1
+2
+3
+```
