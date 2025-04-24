8be2a46150c65efccf2e7a87fe166e9028a10fd0
pratik deshpande <pratik.deshpande90@gmail.com>
2025-04-24 12:52:05 -0500

    Address review comments for src/phone_formatter/__init__.py

--- src/phone_formatter/__init__.py
+++ src/phone_formatter/__init__.py
@@ -1,1 +1,6 @@
-from .phone_formatter import PhoneFormatter
+import phone_formatter from phone_formatter
+
+try:
+    phone_formatter = PhoneFormatter()
+except Exception as e:
+    print(f'Error occurred while initializing PhoneFormatter: {e}')
